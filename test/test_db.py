38df598738e242ed37ee089e66a86050f5d33e50
Peter Strong <peterstrongdev@outlook.com>
2023-10-30 10:36:41 -0400

    Started unit tests

--- test/test_db.py
+++ test/test_db.py
@@ -1,1 +1,12 @@
 import unittest
+import sqlite3
+
+conn = sqlite3.connect("../pfit.db")
+curs = conn.cursor()
+
+
+class TestDatabase(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
