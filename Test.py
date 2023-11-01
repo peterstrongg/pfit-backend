0071731bfe0d3fdf8d41f3a196105a4fe657882d
Peter Strong <peterstrongdev@outlook.com>
2023-11-01 10:27:36 -0400

    Added test for user signup

--- Test.py
+++ Test.py
@@ -1,10 +1,26 @@
 import unittest
+import sqlite3
 from Database import Database
 
 db = Database("pfit.db")
 
-class TestDatabase(unittest.TestCase):
-    pass
+# For cleanup routines after tests complete
+conn = sqlite3.connect("pfit.db")
+curs = conn.cursor()
 
-if __name__ == '__main__':
+# Cleanup routines after tests conclude
+def cleanup_users():
+    curs.execute(
+        "DELETE FROM users WHERE username = ?", 
+        ["pfitDemo"]
+    )
+    conn.commit()
+
+class TestDatabase(unittest.TestCase):
+    def test_signup(self):
+        self.assertTrue(db.add_user("pfitDemo", "password"))
+        cleanup_users()
+
+if __name__ == "__main__":
     unittest.main()
+    
