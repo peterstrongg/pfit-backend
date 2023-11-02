a8fffcd43106c5bda08946b81178c46eaf6892d5
Peter Strong <peterstrongdev@outlook.com>
2023-11-02 08:42:43 -0400

    Added test for share_workout table

--- Test.py
+++ Test.py
@@ -16,10 +16,21 @@ def cleanup_users():
     )
     conn.commit()
 
+def cleanup_sw():
+    curs.execute(
+        "DELETE from share_workout WHERE logging_id = ?",
+        [-1]
+    )
+    conn.commit()
+
 class TestDatabase(unittest.TestCase):
     def test_signup(self):
         self.assertTrue(db.add_user("pfitDemo", "password"))
         cleanup_users()
+
+    def test_share_workout(self):
+        self.assertTrue(db.share_workout(1, -1, "Comment"))
+        cleanup_sw()
 
 if __name__ == "__main__":
     unittest.main()
